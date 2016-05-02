d165e6966de9fd04819dc4eb0d80f4224c8651c1
topkayas <topkayas@os-class.engr.oregonstate.edu>
2016-05-01 20:41:52 -0700

    writing 2

---
+++ Writing/Writing2/scatter.c
@@ -0,0 +1,10 @@
+// tHIS IS THE readfilescatter function THAT IS USED BY THE SCATHER/GATHER I/O
+
+BOOL WINAPI ReadFileScatter(
+  _In_       HANDLE               hFile,
+  _In_       FILE_SEGMENT_ELEMENT aSegmentArray[],
+  _In_       DWORD                nNumberOfBytesToRead,
+  _Reserved_ LPDWORD              lpReserved,
+  _Inout_    LPOVERLAPPED         lpOverlapped
+);
+
