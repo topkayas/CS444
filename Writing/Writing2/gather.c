7d0acc5da23ae2b47345470b9b27deb82fda8417
topkayas <topkayas@os-class.engr.oregonstate.edu>
2016-05-01 23:24:06 -0700

    updated writing assignment

--- Writing/Writing2/gather.c
+++ Writing/Writing2/gather.c
@@ -1,9 +1,10 @@
-// This is the ReadFileScatter function that is used for Scatter/Gather I/O
-BOOL WINAPI ReadFileScatter(
+//This is the WriteFileGather function that is used for Scatter/Gather I/O
+BOOL WINAPI WriteFileGather(
   _In_       HANDLE               hFile,
   _In_       FILE_SEGMENT_ELEMENT aSegmentArray[],
-  _In_       DWORD                nNumberOfBytesToRead,
+  _In_       DWORD                nNumberOfBytesToWrite,
   _Reserved_ LPDWORD              lpReserved,
   _Inout_    LPOVERLAPPED         lpOverlapped
 );
+
 
